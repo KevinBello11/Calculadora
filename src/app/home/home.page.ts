f37683402760e25f20ab4830d871803431368496
KevinBello11 <kebelloc@itc.edu.co>
2023-09-05 15:05:28 -0500

    Subiendo calculadora falta diseño

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,19 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  display ="0"
+  display = "0";
   operand = 0;
   operator = '';
-  memory:any = [];
+  memory: any = [];
+  showTrigFunctions = false; // Variable para controlar la visibilidad de las funciones trigonométricas
+  trigModeDegrees = true; // Variable para controlar el modo de grados/radianes
 
   constructor() {}
 
-  numbers(n: any){
+  numbers(n: any) {
     this.memory.push(n);
     this.display = this.memory.join('');
   }
 
-  operation(op: any){
+  operation(op: any) {
     this.operand = Number(this.display);
     this.memory = [];
     this.operator = op;
@@ -39,27 +41,49 @@ export class HomePage {
     }
   }
 
-  clear(){
+  clear() {
     this.display = '0';
     this.operand = 0;
     this.memory = [];
   }
 
-  addDecimal() {
-    if (!this.display.includes('.')) {
-      this.display += '.';
-    }
-  }
-
   toggleSign() {
     if (this.display !== '0') {
       if (this.display.startsWith('-')) {
-        this.display = this.display.substring(1); 
+        this.display = this.display.substring(1);
       } else {
-        this.display = '-' + this.display; 
+        this.display = '-' + this.display;
       }
     }
   }
-  
-  
+
+  trigFunction(func: string) {
+    let value = Number(this.display);
+
+    if (this.trigModeDegrees) {
+      // Si estamos en modo grados, convertir a radianes antes de calcular
+      value = (value * Math.PI) / 180;
+    }
+
+    switch (func) {
+      case 'sin':
+        this.display = String(Math.sin(value));
+        break;
+      case 'cos':
+        this.display = String(Math.cos(value));
+        break;
+      case 'tan':
+        this.display = String(Math.tan(value));
+        break;
+      case 'π':
+        this.display = String(Math.PI);
+        break;
+      default:
+        break;
+    }
+  }
+
+  toggleTrigMode() {
+    this.trigModeDegrees = !this.trigModeDegrees; // Alternar entre grados y radianes
+  }
 }
